9168b15e0db0f2047fe5d385edf39c837a376976
theodesjardins <theo.desjardins@orange.fr>
2019-02-26 17:54:28 +0100

    adding of last front

---
+++ app/src/main/java/fr/isen/chakouri/pourcombien/Activities/QuestionActivity.kt
@@ -0,0 +1,28 @@
+package fr.isen.chakouri.pourcombien.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import fr.isen.chakouri.pourcombien.R
+import kotlinx.android.synthetic.main.activity_first_choice.*
+import kotlinx.android.synthetic.main.activity_question.*
+
+class QuestionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val seekValue0 = progress + 1
+                textNumber1.text = "$seekValue0"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+    }
+}
