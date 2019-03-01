ab050a5f41734abbaa7993c24c24fb987ca31ff9
Adel Chakouri <adel.chakouri83@gmail.com>
2019-03-01 11:53:00 +0100

    bouton ajout de joueur

--- app/src/main/java/fr/isen/chakouri/pourcombien/Activities/HomeActivity.kt
+++ app/src/main/java/fr/isen/chakouri/pourcombien/Activities/HomeActivity.kt
@@ -1,22 +1,52 @@
 package fr.isen.chakouri.pourcombien.Activities
 
-import android.content.Intent
+import android.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import fr.isen.chakouri.pourcombien.R
+import android.widget.LinearLayout
+import android.view.ViewGroup
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var numberOfLines = 4
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(fr.isen.chakouri.pourcombien.R.layout.activity_home)
 
-        //button play
-        buttonPlay.setOnClickListener {
-            val intent = Intent(this, ModeActivity::class.java)
-            startActivity(intent)
+        addFieldButton.setOnClickListener{
+            addLine()
+
         }
-
     }
+
+    private fun createEditText(): EditText {
+        val lparams = ActionBar.LayoutParams(698, 170) // Width , height
+        val edittext = EditText(this)
+        edittext.layoutParams = lparams
+        edittext.setHint(" Joueur  " + numberOfLines)
+        return edittext
+    }
+
+    fun addLine() {
+        val p = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        createEditText().layoutParams = p
+        createEditText().id = numberOfLines + 1
+        joueursLayout.addView(createEditText())
+        numberOfLines++
+    }
+    /*
+    fun onAddField(v: View) {
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val editText =
+        // Add the new row before the add field button.
+        parentLinearLayout!!.addView(rowView, parentLinearLayout!!.childCount - 1)
+    }
+
+    fun onDelete(v: View) {
+        parentLinearLayout!!.removeView(v.getParent() as View)
+    }
+    */
 }
